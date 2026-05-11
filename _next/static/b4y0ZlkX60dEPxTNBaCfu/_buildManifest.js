84801e65e6b46d254b96060dfc7d51ef76929a6a
MEL <MEL@example.com>
2026-05-11 22:47:41 +0300

    Updates

--- _next/static/b4y0ZlkX60dEPxTNBaCfu/_buildManifest.js
+++ _next/static/b4y0ZlkX60dEPxTNBaCfu/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-site//_next/:path+",
+        "destination": "/portfolio-site/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
