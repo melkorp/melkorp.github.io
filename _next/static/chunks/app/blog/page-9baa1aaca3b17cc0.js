b901c1f3287977d0be7a6a1ef9905c57efae498c
MEL <MEL@example.com>
2026-05-14 23:12:16 +0300

    Updates

---
+++ _next/static/chunks/app/blog/page-9baa1aaca3b17cc0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831],{1503:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2372,23))}},e=>{e.O(0,[372,241,58,358],()=>e(e.s=1503)),_N_E=e.O()}]);
