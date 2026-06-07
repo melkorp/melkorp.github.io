2038a21e221cd319607e86ecb9a99f8e13993040
melkorp <tamogoghi@gmail.com>
2026-06-07 15:32:44 +0300

    Updates

---
+++ _next/static/chunks/app/blog/page-a0c689a3c02f2ab1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[831],{5792:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,647,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[647,448,621,358],()=>s(5792)),_N_E=e.O()}]);
